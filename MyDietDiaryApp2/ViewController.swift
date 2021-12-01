1f3983768cd222ae43091dfd587437780e3833f6
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-12-01 21:50:09 +0900

    Initial Commit

---
+++ MyDietDiaryApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDietDiaryApp2
+//
+//  Created by 橋本純一 on 2021/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
