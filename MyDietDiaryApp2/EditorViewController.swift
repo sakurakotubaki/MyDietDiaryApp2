10a495205720ff45f1a5a80c69b40f1f5709322a
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-12-06 23:27:02 +0900

    日付データ入力まで終了

--- MyDietDiaryApp2/EditorViewController.swift
+++ MyDietDiaryApp2/EditorViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class EditorViewController: UIViewController {
     @IBOutlet weak var inputWeightTextField: UITextField!
+    @IBOutlet weak var inputDateTextField: UITextField!
+    
+    var datePicker: UIDatePicker {
+        let datePicker: UIDatePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.timeZone = .current
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ja-JP")
+        datePicker.date = Date()
+        datePicker.addTarget(self, action: #selector(didChangeDate), for: .valueChanged)
+        return datePicker
+    }
+    
+    var toolBar: UIToolbar {
+        let toolBarRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35)
+        let toolBar = UIToolbar(frame: toolBarRect)
+        let doneItem = UIBarButtonItem(barButtonSystemItem:.done, target: self, action: #selector(didTapDone))
+        toolBar.setItems([doneItem], animated: true)
+        return toolBar
+    }
+    
+    var dateFormatter: DateFormatter {
+        let dateFormatt = DateFormatter()
+        dateFormatt.dateStyle = .long
+        dateFormatt.timeZone = .current
+        dateFormatt.locale = Locale(identifier: "ja-JP")
+        return dateFormatt
+    }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         configureWeightTextField()
+        configureDateTextField()
     }
     
     @objc func didTapDone() {
@@ -19,10 +49,16 @@ class EditorViewController: UIViewController {
     }
     
     func configureWeightTextField() {
-        let toolBarRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35)
-        let toolBar = UIToolbar(frame: toolBarRect)
-        let doneItem = UIBarButtonItem(barButtonSystemItem:.done, target: self, action: #selector(didTapDone))
-        toolBar.setItems([doneItem], animated: true)
         inputWeightTextField.inputAccessoryView = toolBar
+    }
+    
+    func configureDateTextField() {
+        inputDateTextField.inputView = datePicker
+        inputDateTextField.inputAccessoryView = toolBar
+        inputDateTextField.text = dateFormatter.string(from: Date())
+    }
+    
+    @objc func didChangeDate(picker: UIDatePicker) {
+        inputDateTextField.text = dateFormatter.string(from: picker.date)
     }
 }
