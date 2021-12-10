9e2dfbd1b10f7f3bd8daff23041d69dc5e0349e4
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-12-10 21:25:38 +0900

    グラフ画面作成のコードを全て書き終えた

--- MyDietDiaryApp2/GraphViewController.swift
+++ MyDietDiaryApp2/GraphViewController.swift
@@ -6,5 +6,36 @@
 //
 
 import UIKit
+import Charts
+import RealmSwift
 
-class GraphViewController: UIViewController {}
+class GraphViewController: UIViewController {
+    @IBOutlet weak var graphView: LineChartView!
+    
+    var recordList: [WeightRecord] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setRecord()
+        updateGraph()
+    }
+    
+    func setRecord() {
+        let realm = try! Realm()
+        var result = Array(realm.objects(WeightRecord.self))
+        result.sort(by: { $0.date < $1.date})
+        recordList = result
+    }
+    
+    func updateGraph() {
+        var entry = [ChartDataEntry]()
+        recordList.enumerated().forEach({ index, record in
+            let data = ChartDataEntry(x: Double(index), y: record.weight)
+            entry.append(data)
+        })
+        let dataSet = LineChartDataSet(entries: entry, label: "体重")
+        graphView.data = LineChartData(dataSet: dataSet)
+        graphView.data?.notifyDataChanged()
+        graphView.notifyDataSetChanged()
+    }
+}
